ec1a1d7f4a908f459d521671827cd4cbdfc0a1d8
Max Tost <m.luke.tost@gmail.com>
2024-10-15 11:59:13 +0200

    Removed unimportant batch file

--- UV_Notebook.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Navigate to your project directory
-cd C:\Users\Max Tost
-
-REM Use PowerShell to activate the virtual environment and run Jupyter Notebook
-powershell -Command ". .\.venv\Scripts\Activate; jupyter notebook"
