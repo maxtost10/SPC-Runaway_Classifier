6386a463c21ad608306b53dce7335b361ec79997
Max Tost <m.luke.tost@gmail.com>
2025-01-22 15:34:21 +0100

    Random shuffling to find problems statistically

--- Server_Scripts/remote_processing.py
+++ Server_Scripts/remote_processing.py
@@ -5,6 +5,7 @@ import cPickle as pickle
 import numpy as np
 import tempfile
 import shutil
+import random
 
 def convert_to_standard_format(data):
     """
@@ -129,8 +130,8 @@ def main():
     # Filter files to include only JET .mat and .h5 files
     jet_files = [file for file in remote_files if 'JET' in file and (file.endswith('.mat') or file.endswith('.h5'))]
 
-    # jet_files = jet_files[:20]  # Limit the number of files to process for testing
-    jet_files = ['JETno80706.mat']  # Process a single file for testing
+    random.shuffle(jet_files)  # Shuffle the list to get a statistical value
+    jet_files = jet_files[:100]  # Limit the number of files to process for testing
 
     for file_name in jet_files:
         shot_number = file_name.split('.')[0]  # Extract shot number from file name
